34186840594d91be545f7117076f9f08368d0c4e
Sérgio Santos <sergiorobsantos@gmail.com>
2022-03-28 23:11:54 -0300

    add project files

---
+++ auto-layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  auto-layout
+//
+//  Created by Sergio Santos on 28/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
